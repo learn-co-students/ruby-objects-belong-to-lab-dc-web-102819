5acd5150b185e4aca6c70cea751530c11e4deae0
remiodufuye <remi.odufuye@gmail.com>
2019-10-31 10:50:59 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+
+class Post 
+
+    attr_accessor :title , :author
+
+    # def initialize(title = "Hello World" , author = nil )
+    #  @title = title
+    #  @author = author
+    # end 
+     
+
+end 
